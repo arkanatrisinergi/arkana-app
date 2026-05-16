b4de6f4878a72fc5f0993fa2e6043222eff32de7
arkanatrisinergi <arkanatrisinergi@gmail.com>
2026-05-16 22:51:32 +0800

    Update config.js
    update apps script

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // Update ARKANA_SCRIPT_URL setiap kali deploy Apps Script baru
 // ═══════════════════════════════════════════════════════════
 
-const ARKANA_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwU3TF8Rv5sIBdg5Oa8cSVy1E4aXTSIgGIBYMklRmRwm9WlZ6hOcFiEh_83-UkAbPcXMg/exec';
+const ARKANA_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwDAqt_sZTPvg09-SLpJNkuJAvnoyO9g2FQzbrfnr_0NSMYds1vaD3tfEpdAn_NptaOqQ/exec';
